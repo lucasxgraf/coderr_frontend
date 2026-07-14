47dc6de58b2cf816d7c462416fc676e2a63ff748
Lucas Graf <lucasgraf01@gmail.com>
2026-07-14 11:19:37 +0200

    fix: update API and static base URLs to production

--- shared/scripts/config.js
+++ shared/scripts/config.js
@@ -9,8 +9,8 @@ const GUEST_LOGINS = {
     }
 }
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api/';
-const STATIC_BASE_URL = 'http://127.0.0.1:8000/';
+const API_BASE_URL = 'https://coderr-api.lucasgraf.com/api/';
+const STATIC_BASE_URL = 'https://coderr-api.lucasgraf.com/';
 
 
 const LOGIN_URL = 'login/';
